8036cac25f971589fe89d41f2314cd0b895ae044
Tibor Lapos <lapostibi@gmail.com>
2020-12-14 08:50:15 +0100

    Zmenil som hodnoti na prepojenie s databazou

--- mysql_connect.php
+++ mysql_connect.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', '');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'test');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'b17lapos');
+define('DB_PASSWORD', '@iaDsH4PgN2wX');
+define('DB_NAME', 'b17lapos');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
